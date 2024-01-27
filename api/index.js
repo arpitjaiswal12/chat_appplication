07d323142bc23c83b3df4f3aca1b6c8f849ca336
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-01-27 20:56:33 +0530

    modified best index.js file sockets working correctly

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import dbConnect from "./config/database.js";
+import jwt from "jsonwebtoken";
 import path from "path";
 import authRoute from "./routes/auth_route.js";
 import msgRoute from "./routes/msg_route.js";
@@ -15,38 +16,31 @@ dbConnect(); //connection with database
 const __dirname = path.resolve();
 const app = express();
 
-// middleware to parse json request body
 app.use(express.json());
 
 app.use(cookieParser());
 
-//mounts (adding/ appending the ) todo API routes :: when user hits the user the bydefault path is atteached with the entered request
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", msgRoute);
-
-
-app.listen(3001, () => {
-  console.log(`Server is started at port 3001`);
-});
-
-
-// conncetion with real time chat application 
-// using socket.io
 
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 global.onlineUsers = new Map();
+
 io.on("connection", (socket) => {
+  // console.log(socket.id);
   global.chatSocket = socket;
+
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
   });
@@ -57,4 +51,8 @@ io.on("connection", (socket) => {
       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
   });
+});
+
+server.listen(3001, () => {
+  console.log(`Server is started at port 3001`);
 });
